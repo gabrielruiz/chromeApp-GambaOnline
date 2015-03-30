4ff8c3031367e1a81dbc21d9f3a58dafdd376c31
Gabriel Ruiz <gabrielruiz@gene-sis.com.ar>
2015-03-30 01:33:18 -0300

    Commit: Add a second URL to listen the radio when one is failing. Also improve error handled and add logs in order to identify futures issues. Author: GabrielRuiz

--- background.js
+++ background.js
@@ -9,36 +9,43 @@ chrome.browserAction.getTitle({}, function(title){
 });
 
 
-var timeInterval = (60 * 1000) * 60,
-    radioState = 'pause';
+var data = {
+        "streamUrl" : "http://gambafm.lorini.net:10630/;",
+        "streamUrl2": "http://www.fm.net.ve:9000/GAMBAFM",
+        "streamType": "mp3"
+    },
+    streamMedia = new Object(),
+    timeInterval = (60 * 1000) * 60,
+    timeReconnection = (60 * 1000) * 15,
+    radioState = 'pause',
+    timeOut,
+    reconnect = 0;
 
 
-function initPlayer(data) {
+function initPlayer() {
 
-    var streamMedia = new Object(),
-        jplayer_1 = $("#jquery_jplayer_1"),
+    var jplayer_1 = $("#jquery_jplayer_1"),
         ready = false,
-        timeOut;
+        playerState;
 
     streamMedia[data['streamType']] = data['streamUrl'];
     jplayer_1.jPlayer({
     ready: function () {
-        ready = true;
         $(this).jPlayer("setMedia", streamMedia );
-        console.log("playerready")
+        ready = true;
+        playerState = "ready";
+        console.log("loading player");
     },
     play: function(event){
+        SetTimeOut();
+        playerState = "play";
         console.log("playing now");
-        timeOut = setTimeout(function () {
-            console.log('Set timeout');
-            clearStreamBuffered(streamMedia); 
-        }, timeInterval);
     },
     pause: function(event) {
-        //kill the media to change the "pause" normal action.
+        //kill the media to change the "pause" normal action.        
         jplayer_1.jPlayer("clearMedia");
-        clearTimeout(timeOut);
-        timeOut = null;
+        ClearTimeOut();
+        playerState = "pause";
         console.log("pausing the player");
     },
 
@@ -46,6 +53,13 @@ function initPlayer(data) {
         //check if the user clicked on the play button after a pause interval.
         if (ready && event.jPlayer.error.type == $.jPlayer.error.URL_NOT_SET) {
             jplayer_1.jPlayer("setMedia", streamMedia).jPlayer("play");
+            console.log('ERROR URL NOT SET');
+            return;
+        }
+        
+        if(ready && event.jPlayer.error.type == $.jPlayer.error.URL) {
+            ReConnect(jplayer_1, playerState);
+            console.log('ERROR URL NOT RESPONSE');
         }
     },
     swfPath: "jplayer",
@@ -59,30 +73,78 @@ function initPlayer(data) {
 
 function clearStreamBuffered(streamMedia) {
     $("#jquery_jplayer_1").jPlayer("setMedia", streamMedia).jPlayer("play");
-    setTimeout(function () { clearStreamBuffered(streamMedia); }, timeInterval);
 }
 
-$(document).ready(function(){
+function SetTimeOut(METHOD, TIMEINTERVAL) {
 
-    var data = {
-    "streamUrl" : "http://gambafm.lorini.net:10630/;",
-    "streamType": "mp3"
-    };
-    initPlayer(data);
+    ClearTimeOut();
+
+    var time = (typeof TIMEINTERVAL !== 'undefined') ? TIMEINTERVAL : timeInterval;
+    
+    if (typeof METHOD !== 'undefined') {
+        timeOut = setTimeout(function () {
+            console.log('Running timeout');
+            METHOD(); 
+        }, time);
+        return;
+    }
+
+    timeOut = setTimeout(function () {
+        console.log('Running timeout');
+        clearStreamBuffered(streamMedia); 
+    }, time);
+
+    console.log('Set timeout');
+}
+
+function ClearTimeOut() {
+    clearTimeout(timeOut);
+    timeOut = null;
+    console.log('Clear timeout');
+}
+
+function ReConnect(JPLAYER, STATE) {
+
+    var state = (typeof STATE !== 'undefined') ? STATE : "play";
+
+    if(reconnect === 5) {
+        reconnect = 0;
+        SetTimeOut(function () {
+            ReConnect(JPLAYER, STATE);
+        }, timeReconnection);
+        return;
+    }
+
+    if (reconnect%2 === 0) {
+        streamMedia[data['streamType']] = data['streamUrl'];
+        JPLAYER.jPlayer("setMedia", streamMedia).jPlayer(state);
+        
+    }
+    else {
+        streamMedia[data['streamType']] = data['streamUrl2'];
+        JPLAYER.jPlayer("setMedia", streamMedia).jPlayer(state);
+    }
+
+    reconnect++;
+}
+
+$(document).ready(function() {
+
+    initPlayer();
 
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
 
     if (radioState === 'pause') {
-        radioState = 'play';
         chrome.browserAction.setIcon({path:"iconPlay.png"});
         chrome.browserAction.setTitle({title:"Playing Gamba FM"});
+        radioState = 'play';
     }
     else {
-        radioState = 'pause';
         chrome.browserAction.setIcon({path:"iconPause.png"});
         chrome.browserAction.setTitle({title:"Paused Gamba FM"});
+        radioState = 'pause';
     }
 
     $("#jquery_jplayer_1").jPlayer(radioState);
